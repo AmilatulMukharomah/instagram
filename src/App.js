edb92618a1ce3905f73ed0c65098644a5d54144e
Mila <47048357+MilaKisaragi@users.noreply.github.com>
2021-03-11 09:48:14 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import img1 from './img/navigation.png'
+import img2 from './img/home.png'
+import img3 from './img/dm.png'
+import img4 from './img/like.png'
+import img5 from './img/profile.png'
 
 const App = () => {
   return (
-    <div className="navbar">
-      <div className="satu">
-        INSTAGRAM
+    <div >
+      <div className="navbar">
+        <div className="satu">
+          INSTAGRAM
+        </div>
+        <div className="dua">
+          <form >
+            <img className="cari" src={img1} alt="avatar" />
+            <input type="text" placeholder="Search.." name="search" />
+          </form>
+        </div>
+        <div className="tiga">
+          <img className="iconnav" src={img2} alt="avatar" />
+          <img className="iconnav" src={img3} alt="avatar" />
+          <img className="iconnav" src={img1} alt="avatar" />
+          <img className="iconnav" src={img4} alt="avatar" />
+          <img className="iconnav" src={img5} alt="avatar" />
+        </div>
       </div>
-      <div className="dua">
-        <form >
-          <img className="cari" src={img1} alt="avatar" />
-          <input type="text" placeholder="Search.." name="search" />
-        </form>
-      </div>
-      <div className="tiga">
+      <div className="konten">
 
       </div>
-
     </div>
+
   );
 }
 
